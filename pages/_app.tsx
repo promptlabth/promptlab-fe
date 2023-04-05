e84fb672f0450b8e92e2932619c76669f1f10336
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-05 02:21:08 +0000

    update seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,22 +26,12 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page)
     return getLayout(
-        <>
-            <Head>
-                <title>Prompt Lab: AI-Powered Content Generator for Social Media & More</title>
-                <meta
-                    name="description"
-                    content="Prompt Lab is an AI-powered content generation tool that helps you create engaging social media posts, emails, and more in English and Thai. Boost your content creation with Prompt Lab today! - เป็นเครื่องมือสร้างเนื้อหาที่ขับเคลื่อนด้วย AI ซึ่งช่วยคุณสร้างโพสต์สื่อสังคม, อีเมล, และอื่น ๆ ที่น่าสนใจในภาษาอังกฤษและภาษาไทย ปรับปรุงการสร้างเนื้อหาของคุณด้วย Prompt Lab ในวันนี้!"
-                />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <html lang="th" />
-            </Head>
-            <main className={noto_sans_thai.className}>
+        <main className={noto_sans_thai.className}>
 
-                <Script id='gtag-id' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-958P0ZZK61`} />
+            <Script id='gtag-id' strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-958P0ZZK61`} />
 
-                <Script id='gtag-id-engine' strategy="lazyOnload">
-                    {`
+            <Script id='gtag-id-engine' strategy="lazyOnload">
+                {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -49,19 +39,27 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
                     page_path: window.location.pathname,
                     });
                 `}
-                </Script>
-                <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></Script>
-                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossOrigin="anonymous"></Script>
-                {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+            </Script>
+            <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></Script>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossOrigin="anonymous"></Script>
+            {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap" rel="stylesheet" /> */}
-                <LanguageProvider>
-                    <AppNavbar />
-                    <Component {...pageProps} />
-                    <Footer />
-                </LanguageProvider>
-            </main>
-        </>
+            <LanguageProvider>
+                <Head>
+                    <title>Prompt Lab: AI-Powered Content Generator for Social Media & More</title>
+                    <meta
+                        name="description"
+                        content="Prompt Lab is an AI-powered content generation tool that helps you create engaging social media posts, emails, and more in English and Thai. Boost your content creation with Prompt Lab today! - เป็นเครื่องมือสร้างเนื้อหาที่ขับเคลื่อนด้วย AI ซึ่งช่วยคุณสร้างโพสต์สื่อสังคม, อีเมล, และอื่น ๆ ที่น่าสนใจในภาษาอังกฤษและภาษาไทย ปรับปรุงการสร้างเนื้อหาของคุณด้วย Prompt Lab ในวันนี้!"
+                    />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <html lang="th" />
+                </Head>
+                <AppNavbar />
+                <Component {...pageProps} />
+                <Footer />
+            </LanguageProvider>
+        </main>
     )
 
     // return getLayout(<Component {...pageProps} />)
