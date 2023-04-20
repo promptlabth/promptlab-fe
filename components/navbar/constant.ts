f39a2a59513da7d9508cfacd66c0f1584bdb074c
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-20 02:34:44 +0000

    remove create email

--- components/navbar/constant.ts
+++ components/navbar/constant.ts
@@ -1,13 +1,7 @@
-// export const navbarConstans = {
-//      sellingPostTitle: "createSellingPost.title",
-//      createEmailTitle: "createEmail.title",
-//      createArticalTitle: "createArticle.title",
-//      createScriptsTitle: "createVideoScripts.title",
-// }
 
 export const urlLinks = [
      { titleKey: 'createSellingPost.title', href: '/createSellingPost' },
-     { titleKey: 'createEmail.title', href: '/createEmail' },
+    //  { titleKey: 'createEmail.title', href: '/createEmail' },
      { titleKey: 'createArticle.title', href: '/createArticle' },
      { titleKey: 'createScripts.title', href: '/createShortVideoScripts' },
      { titleKey: 'createClickBait.title', href: '/createClickBaitWord' },
