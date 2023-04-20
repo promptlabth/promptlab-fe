48150cf581c4900a7c63c6169c7d4c8ee367eca8
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-04-20 04:55:05 +0000

    remove create email

--- components/navbar/constant.ts
+++ components/navbar/constant.ts
@@ -1,11 +1,10 @@
 
 export const urlLinks = [
      { titleKey: 'createSellingPost.title', href: '/createSellingPost' },
-    //  { titleKey: 'createEmail.title', href: '/createEmail' },
+     { titleKey: 'createContents.title', href: '/createIdeaContent'},
      { titleKey: 'createArticle.title', href: '/createArticle' },
      { titleKey: 'createScripts.title', href: '/createShortVideoScripts' },
      { titleKey: 'createClickBait.title', href: '/createClickBaitWord' },
      // { titleKey: 'navbar.title.createImage', href: '/createImage' },
-     { titleKey: 'createContents.title', href: '/createIdeaContent'}
  ];
 
