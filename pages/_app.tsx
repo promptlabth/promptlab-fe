4f0941a478668c9289b85c9edeed01989f2ccac5
HanawuZ <b6302245@gmail.com>
2023-05-17 17:48:59 +0700

    add new footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Footer from '@/components/Footer';
+import Footer from '@/components/footer/Footer';
 import { AppNavbar } from '@/components/navbar/Navbar';
 import { LanguageProvider } from '@/language/ LanguageContext';
 
