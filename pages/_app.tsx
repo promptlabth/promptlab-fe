5d7840df9c590e94f9d39a9864fe596365f75618
HanawuZ <b6302245@gmail.com>
2023-07-08 15:42:09 +0700

    fix : useEffect in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     if (accessToken) {
       setToken(accessToken)
     }
-  })
+  },[])
   return getLayout(
     <main className={noto_sans_thai.className}>
       <Script
