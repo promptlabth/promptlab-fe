3bc23a90c921e1e18147e8bade522f8a34d81440
HanawuZ <b6302245@gmail.com>
2023-08-28 14:47:10 +0700

    create add coin button

--- components/navbar/NavbarMobileAfterLogin.tsx
+++ components/navbar/NavbarMobileAfterLogin.tsx
@@ -15,7 +15,8 @@ import { MdSell, MdOutlineArticle } from "react-icons/md";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { FaClosedCaptioning } from "react-icons/fa";
 import { BsFillCircleFill } from "react-icons/bs"
-import {BiLogOut} from "react-icons/bi"
+import { BiLogOut } from "react-icons/bi"
+import { AiFillPlusCircle } from "react-icons/ai"
 import { useUserContext } from "@/contexts/UserContext";
 export const NavbarMobileAfterLogin: React.FC = () => {
   const { language, setLanguage } = useLanguage();
@@ -109,7 +110,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
         >
           {windowWidth < 992 ? (
             <ul className="navbar-nav mt-auto mb-auto ms-auto mb-lg-0 ">
-              <li className={`${styles.profile} nav-item text-center`}>
+              <li className={`${styles.profile} nav-item text-center `}>
                 <div className="pt-4">
                   <img className={`${styles.user_profile_pic}`} src={userContext?.user?.profilepic} alt="profic-pic" />
                   <b>
@@ -117,7 +118,19 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                   </b>
                 </div>
               </li>
-              <li className="nav-item pt-3">
+              <div className="pt-3 d-flex justify-content-between p-2 align-items-center">
+                <div className="ms-4 d-flex align-items-center">
+                  <BsFillCircleFill size={20} className={`${styles.coin}`} />
+                  <div className="px-2 text-white user-select-none fs-5">100000</div>
+                </div>
+                <Link href={"/"} className={`${styles.add_coin_button} me-4 fs-5`}>
+                  เติมเหรียญ
+                </Link>
+              </div>
+              <li>
+                <hr style={{ color: "white" }}></hr>
+              </li>
+              <li className="nav-item">
                 <div className="nav-link">
                   <button className={styles.navbar_help_button}>
                     <AiFillHome />
@@ -242,9 +255,14 @@ export const NavbarMobileAfterLogin: React.FC = () => {
           ) : (
             <ul className="d-flex align-items-center navbar-nav mt-auto mb-auto ms-auto mb-lg-0">
               <li className="nav-item">
-                <div className="nav-link">
+                <div className="nav-link d-flex align-items-center">
                   <BsFillCircleFill size={20} className={`${styles.coin}`} />
-                  Coming soon
+                  <div className="px-2 user-select-none">100000</div>
+                  <Link
+                    href={"/payments"}
+                  >
+                    <AiFillPlusCircle className={`${styles.add_coin}`} size={34} />
+                  </Link>
                 </div>
               </li>
 
@@ -257,9 +275,9 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  {language === "th" && <Flag country="TH" className = {`${styles.flag_size}`} />}
-                  {language === "eng" && <Flag country="US" className = {`${styles.flag_size}`} />}
-                  {language === "id" && <Flag country="ID" className = {`${styles.flag_size}`} />}
+                  {language === "th" && <Flag country="TH" className={`${styles.flag_size}`} />}
+                  {language === "eng" && <Flag country="US" className={`${styles.flag_size}`} />}
+                  {language === "id" && <Flag country="ID" className={`${styles.flag_size}`} />}
                 </a>
                 <ul
                   className="dropdown-menu dropdown-menu-dark"
@@ -272,7 +290,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         setLanguage("eng");
                       }}
                     >
-                      <Flag country="US" className = {`${styles.flag_size} me-2`} /> English
+                      <Flag country="US" className={`${styles.flag_size} me-2`} /> English
                     </a>
                     <a
                       className={`dropdown-item ${styles.language_list}`}
@@ -280,7 +298,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         setLanguage("th");
                       }}
                     >
-                      <Flag country="TH" className = {`${styles.flag_size} me-2`}/> Thai
+                      <Flag country="TH" className={`${styles.flag_size} me-2`} /> Thai
                     </a>
                     <a
                       className={`dropdown-item ${styles.language_list}`}
@@ -288,20 +306,11 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         setLanguage("id");
                       }}
                     >
-                      <Flag country="ID" className = {`${styles.flag_size} me-2`}/> Indonesia
+                      <Flag country="ID" className={`${styles.flag_size} me-2`} /> Indonesia
                     </a>
                   </li>
                 </ul>
               </li>
-              {/* <li className="nav-item">
-                <div className="nav-link">
-                  <button className={styles.navbar_help_button}>
-                    <Link href={"/"} className={`${styles.remove_underline}`}>
-                      {translate("home.title", language)}
-                    </Link>
-                  </button>
-                </div>
-              </li> */}
               <li className="nav-item">
                 <div className="nav-link">
                   <button className={styles.navbar_help_button}>
@@ -319,9 +328,9 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                   <img className={`${styles.user_profile_pic_desktop}`} src={userContext?.user?.profilepic} alt="profic-pic" />
 
                 </a>
-                <ul 
+                <ul
                   className={`${styles.login_dropdown_menu} border dropdown-menu px-1`}
-                  style={{marginLeft:"-8rem", background:"#33393F"}}
+                  style={{ marginLeft: "-8rem", background: "#33393F" }}
                 >
                   <li><div className="text-white text-center fs-5 fw-semibold text">{userContext?.user?.name}</div></li>
                   <li><hr className="dropdown-divider bg-white" /></li>
@@ -331,7 +340,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         href={"/"}
                         className={`${styles.remove_underline} d-flex align-items-center`}
                       >
-                        <AiFillHome className="m-1 ms-2" size={22}/>
+                        <AiFillHome className="m-1 ms-2" size={22} />
                         <div className="ps-2 pt-1">{translate("home.title", language)}</div>
                       </Link>
                     </a>
@@ -342,7 +351,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         href={"/history"}
                         className={`${styles.remove_underline} d-flex align-items-center`}
                       >
-                        <AiOutlineHistory className="m-1 ms-2" size={22}/>
+                        <AiOutlineHistory className="m-1 ms-2" size={22} />
                         <div className="ps-2 pt-1"> {translate("history", language)}</div>
                       </Link>
                     </a>
@@ -355,36 +364,13 @@ export const NavbarMobileAfterLogin: React.FC = () => {
                         userContext?.handleLogout();
                       }}
                     >
-                      <BiLogOut className="m-1 ms-2" size={20}/>
+                      <BiLogOut className="m-1 ms-2" size={20} />
                       <div className="ps-2 pt-1"> {translate("logout", language)}</div>
                     </a>
                   </li>
                 </ul>
               </li>
-              {/* <li className="nav-item">
-                <div className="nav-link">
-                  <button className={styles.navbar_help_button}>
-                    <Link
-                      href={"/history"}
-                      className={`${styles.remove_underline}`}
-                    >
-                      {translate("history", language)}
-                    </Link>
-                  </button>
-                </div>
-              </li>
-              <li className="nav-item text-center">
-                <div className="nav-link ">
-                  <button
-                    className={`${styles.navbar_logout_button}`}
-                    onClick={() => {
-                      userContext?.handleLogout();
-                    }}
-                  >
-                    {translate("logout", language)}
-                  </button>
-                </div>
-              </li> */}
+
             </ul>
           )}
         </div>
