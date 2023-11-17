07af5fbf3e140a5596cbd5611c9f4cf337c073b3
HanawuZ <b6302245@gmail.com>
2023-11-17 17:33:15 +0700

    change to production

--- api/LoginAPI.ts
+++ api/LoginAPI.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export async function Login(accessToken : string) {
-   const apiUrl = `https://prompt-lab-be-dev-uu4qhhj35a-as.a.run.app/users/login`
+   // const apiUrl = `https://prompt-lab-be-dev-uu4qhhj35a-as.a.run.app/users/login`
    
    // Product Api url
-   // const apiUrl = `https://prompt-lab-be-uu4qhhj35a-as.a.run.app/users/login`
+   const apiUrl = `https://prompt-lab-be-uu4qhhj35a-as.a.run.app/users/login`
    
    try {
       const requestOption = { headers: { "Authorization": `Bearer ${accessToken}` },}
