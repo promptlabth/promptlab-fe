7b410b29b7d461f14c50001f6f4fc722a33ba337
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2024-01-01 16:56:15 +0000

    remove snow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,8 +69,8 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
             integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i"
             crossOrigin="anonymous"
          ></Script>
-         <Script id="snow" defer src="https://app.embed.im/snow.js">
-         </Script>
+         {/* <Script id="snow" defer src="https://app.embed.im/snow.js">
+         </Script> */}
          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&display=swap" rel="stylesheet" /> */}
