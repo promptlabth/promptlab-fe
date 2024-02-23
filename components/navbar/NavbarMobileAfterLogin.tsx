037464e4ba4aeb6230935a1036dac135f04e93b8
HanawuZ <b6302245@gmail.com>
2024-02-23 20:28:38 +0700

    feat: hide history page

--- components/navbar/NavbarMobileAfterLogin.tsx
+++ components/navbar/NavbarMobileAfterLogin.tsx
@@ -286,7 +286,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
             </Link>
           </a>
         </li>
-        <li className=" py-1 ">{history()}</li>
+        {/* <li className=" py-1 ">{history()}</li> */}
         <li className="py-1 ">{logoutDesktopScreen()}</li>
       </ul>
     </li>
@@ -484,7 +484,7 @@ export const NavbarMobileAfterLogin: React.FC = () => {
         </li>
         <li className="nav-item">
                 <div className="nav-link">
-              {historyMobile()}
+              {/* {historyMobile()} */}
            </div>
            </li>
       <li>
